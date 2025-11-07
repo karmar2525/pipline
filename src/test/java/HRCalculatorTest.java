a6d7ad32cac412ae1865ee0feadcb65ed8de7e5a
Karolina Marek <karolina.marek005@gmail.com>
2025-11-07 12:00:06 +0100

    first commit v2

---
+++ src/test/java/HRCalculatorTest.java
@@ -0,0 +1,11 @@
+import org.example.HRCalculator;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+public class HRCalculatorTest {
+    @Test
+    public void testValidMaxHR() {
+        var result = HRCalculator.calculateMaxHR(25);
+        assertEquals(195, result);
+    }
+}
+
