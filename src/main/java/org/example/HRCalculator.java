dc7b1b9f1d0ed0c7214d3cefd7072f08fcdcadc0
Karolina Marek <karolina.marek005@gmail.com>
2025-11-09 12:46:21 +0100

    Fix:all correct age range and package path

--- src/main/java/org/example/HRCalculator.java
+++ src/main/java/org/example/HRCalculator.java
@@ -1,15 +1,16 @@
 package org.example;
 
 public class HRCalculator {
-    public static int calculateMaxHR(int age){
-        if(age>120){
-            throw new IllegalArgumentException(
-                    "Age out of bounds"
-            );
+
+    public static int calculateMaxHR(int age) {
+        if(age < 0 || age > 120) {
+            throw new IllegalArgumentException("Invalid age");
         }
         return 220 - age;
     }
-    public static String getWorkoutZone(int age, int bpm){
+
+    public static String getWorkoutZone(int age, int bpm) {
         return "?";
     }
 }
+
